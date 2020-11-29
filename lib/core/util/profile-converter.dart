62206256e969ec7856a99282588921f6b6475c29
babirms <bramos@id.uff.br>
2020-11-29 17:44:19 -0300

    move profile converter's to its own file

---
+++ lib/core/util/profile-converter.dart
@@ -0,0 +1,36 @@
+import 'package:estuduff/core/resource/colors_estuduff.dart';
+import 'package:estuduff/core/resource/strings_estuduff.dart';
+import 'package:estuduff/features/profile/domain/entity/study_profile_enum.dart';
+import 'package:flutter/material.dart';
+
+class ProfileConverter {
+  static Color recoverProfileColor(StudyProfileEnum profile) {
+    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
+      return ColorsEstudUff.primaryGreen;
+    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
+      return ColorsEstudUff.primaryBlue;
+    } else {
+      return ColorsEstudUff.primaryRed;
+    }
+  }
+
+  static String recoverProfileIcon(StudyProfileEnum profile) {
+    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
+      return StringsEstudUff.available_env_icon;
+    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
+      return StringsEstudUff.wolf_env_icon;
+    } else {
+      return StringsEstudUff.share_env_icon;
+    }
+  }
+
+  static String recoverStudyProfile(StudyProfileEnum profile) {
+    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
+      return StringsEstudUff.jack_profile;
+    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
+      return StringsEstudUff.wolf_profile;
+    } else {
+      return StringsEstudUff.share_profile;
+    }
+  }
+}
--- lib/features/environment/presentation/pages/SingleEnviromentScreen.dart
+++ lib/features/environment/presentation/pages/SingleEnviromentScreen.dart
@@ -3,6 +3,7 @@ import 'package:estuduff/core/resource/dimensions.dart';
 import 'package:estuduff/core/resource/fonts_estuduff.dart';
 import 'package:estuduff/core/resource/strings_estuduff.dart';
 import 'package:estuduff/core/ui/appbar_estuduff.dart';
+import 'package:estuduff/core/util/profile-converter.dart';
 import 'package:estuduff/features/environment/domain/entity/building.dart';
 import 'package:estuduff/features/environment/domain/entity/campus.dart';
 import 'package:estuduff/features/environment/domain/entity/environment.dart';
@@ -12,7 +13,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SingleEnviromentScreen extends StatefulWidget {
-  // TODO: Colocar 'final' e remover a inicialização da var
+  // TODO: Colocar 'final'
   Environment enviroment;
 
   SingleEnviromentScreen({Key key, this.enviroment}) : super(key: key);
@@ -27,19 +28,22 @@ class _SingleEnviromentScreenState extends State<SingleEnviromentScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: remover inicialização de enviroment
     widget.enviroment = new Environment(
+      id: 01,
+      name: 'Biblioteca',
+      complement: 'Rua Passo da Pátria',
+      studyProfile: StudyProfileEnum.LONELY_WOLF,
+      type: StudyPlaceType(id: 01, name: 'Biblioteca'),
+      building: Building(
         id: 01,
+        campus: Campus(id: 01, name: 'PV'),
         name: 'Biblioteca',
-        complement: 'Rua Passo da Pátria',
-        studyProfile: StudyProfileEnum.LONELY_WOLF,
-        type: StudyPlaceType(id: 01, name: 'Biblioteca'),
-        building: Building(
-          id: 01,
-          campus: Campus(id: 01, name: 'PV'),
-          name: 'Biblioteca',
-        ));
+      ),
+    );
 
-    this.enviromentColor = _recoverProfileColor(widget.enviroment.studyProfile);
+    this.enviromentColor =
+        ProfileConverter.recoverProfileColor(widget.enviroment.studyProfile);
     return _buildBody(context);
   }
 
@@ -133,11 +137,11 @@ class _SingleEnviromentScreenState extends State<SingleEnviromentScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   _textInfo(
-                      info:
-                          _recoverStudyProfile(widget.enviroment.studyProfile),
+                      info: ProfileConverter.recoverStudyProfile(
+                          widget.enviroment.studyProfile),
                       label: StringsEstudUff.enviroment_study_profile),
-                  SvgPicture.asset(
-                      _recoverProfileIcon(widget.enviroment.studyProfile)),
+                  SvgPicture.asset(ProfileConverter.recoverProfileIcon(
+                      widget.enviroment.studyProfile)),
                 ],
               ),
             ],
@@ -175,35 +179,5 @@ class _SingleEnviromentScreenState extends State<SingleEnviromentScreen> {
         ],
       ),
     );
-  }
-
-  String _recoverStudyProfile(StudyProfileEnum profile) {
-    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
-      return StringsEstudUff.jack_profile;
-    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
-      return StringsEstudUff.wolf_profile;
-    } else {
-      return StringsEstudUff.share_profile;
-    }
-  }
-
-  String _recoverProfileIcon(StudyProfileEnum profile) {
-    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
-      return StringsEstudUff.available_env_icon;
-    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
-      return StringsEstudUff.wolf_env_icon;
-    } else {
-      return StringsEstudUff.share_env_icon;
-    }
-  }
-
-  Color _recoverProfileColor(StudyProfileEnum profile) {
-    if (profile == StudyProfileEnum.JACK_OF_ALL_TRADES) {
-      return ColorsEstudUff.primaryGreen;
-    } else if (profile == StudyProfileEnum.LONELY_WOLF) {
-      return ColorsEstudUff.primaryBlue;
-    } else {
-      return ColorsEstudUff.primaryRed;
-    }
   }
 }
